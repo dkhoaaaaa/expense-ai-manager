67b33ed38dc3a128138b85d8bc27233a9e5b6ae3
thanhduy-tech <thanhduyhd442@gmail.com>
2026-06-21 10:30:13 +0700

    Add files via upload

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -1,15 +1,21 @@
 from app import db
-from app.models.transaction import Transaction
+from app.models.giaoDichModel import GiaoDich as Transaction
 from sqlalchemy import extract
+from datetime import datetime
 
 
 # ➕ CREATE TRANSACTION
-def create_transaction(data):
+def create_transaction(user_id, data):
     t = Transaction(
+        idTK=user_id,
         amount=data["amount"],
         type=data["type"],  # income / expense
         category_id=data["category_id"],
-        description=data.get("description", "")
+        description=data.get("description", ""),
+        ngayGiaoDich=datetime.strptime(
+            data["ngay_giao_dich"],
+            "%Y-%m-%d"
+        ).date()
     )
 
     db.session.add(t)
@@ -18,34 +24,34 @@ def create_transaction(data):
 
 
 # 📥 GET ALL + FILTER
-def get_transactions(filters=None):
-    query = Transaction.query
-
-    if "day" in filters:
-        query = query.filter(
-            extract("day", Transaction.created_at)
-            == int(filters["day"])
-        )
-
-    if "month" in filters:
-        query = query.filter(
-            extract("month", Transaction.created_at)
-            == int(filters["month"])
-        ) 
+def get_transactions(user_id, filters=None):
+    query = Transaction.query.filter_by(idTK=user_id)
 
     if filters:
-        if "type" in filters:
+        if "day" in filters and filters["day"]:
+            query = query.filter(
+                extract("day", Transaction.created_at)
+                == int(filters["day"])
+            )
+
+        if "month" in filters and filters["month"]:
+            query = query.filter(
+                extract("month", Transaction.created_at)
+                == int(filters["month"])
+            ) 
+
+        if "type" in filters and filters["type"]:
             query = query.filter_by(type=filters["type"])
 
-        if "category_id" in filters:
+        if "category_id" in filters and filters["category_id"]:
             query = query.filter_by(category_id=filters["category_id"])
 
     return query.all()
 
 
 # ❌ DELETE
-def delete_transaction(transaction_id):
-    t = Transaction.query.get(transaction_id)
+def delete_transaction(user_id, transaction_id):
+    t = Transaction.query.filter_by(id=transaction_id, idTK=user_id).first()
 
     if not t:
         return None
@@ -54,13 +60,13 @@ def delete_transaction(transaction_id):
     db.session.commit()
     return True
 
-def search_transactions(keyword):
-    return Transaction.query.filter(
+def search_transactions(user_id, keyword):
+    return Transaction.query.filter_by(idTK=user_id).filter(
         Transaction.description.like(f"%{keyword}%")
     ).all()
 
-def update_transaction(transaction_id, data):
-    t = Transaction.query.get(transaction_id)
+def update_transaction(user_id, transaction_id, data):
+    t = Transaction.query.filter_by(id=transaction_id, idTK=user_id).first()
 
     if not t:
         return None
